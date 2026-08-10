20dfdcb2c2445bbf7758a6621a6e1f0ef94f66b6
yjvvzv74xh-beep <yanorio@gmail.con>
2026-08-09 23:02:25 -0400

    Second Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
-import requests
+import requests, json
+
 #Import the requests library to handle HTTP requests
 
 def emotion_detector(text_to_analyze):
@@ -11,4 +12,17 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json = myobj, headers = header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    output = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': max(emotions, key=emotions.get)
+    }
+
+    print(output)
